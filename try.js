b69cbc7d52d51c91cc7239556f4447bb748a2f95
RotemZilberman <112009232+RotemZilberman@users.noreply.github.com>
2024-06-16 00:26:40 +0300

    Delete try.js

--- try.js
+++
@@ -1,8 +0,0 @@
-outputFiles = {
-    text_translation: 'text_translation.txt',
-    signWriting_translation: 'signWriting_translation.txt',
-    voice_translation: 'voice_translation.txt'
-}
-for (const [responseKey, outputFile] of Object.entries(outputFiles)) {
-    console.log(responseKey,"with", outputFile);
-}
